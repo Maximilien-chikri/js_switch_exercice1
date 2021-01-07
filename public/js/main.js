769952c587bda6893dd71b9bf6e24fd9ab0d0bd7
Maximilien <maximilienchikri@gmail.com>
2021-01-07 09:55:57 +0100

    fin exercice basique (voir si pousser l'exo

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// 1
+let jrsem = prompt("noter un jour de la semaine")
+
+switch (jrsem) {
+    case "lundi":
+        console.log("on est lundi. tu as cours a MG et en soirée tu as sport.");
+        break;
+    default:
+        break;
+}
+// 2
+let age = prompt("tu as quel age")
+
+switch (age) {
+    case "27":
+        console.log("tu as bien plus de 18 ans");
+        break;
+
+    default:
+        break;
+}
